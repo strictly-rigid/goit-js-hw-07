6921b29b98b4310a1a6b97ce0f01e638f2db5ad6
Denys Lavryk <121813691+strictly-rigid@users.noreply.github.com>
2023-05-05 17:15:22 +0300

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createGalleryItems(gallery) {
+   return gallery.map(({preview, original, description}) => {
+        return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </li>
+        `;
+    }).join('');
+
+       }
+
+const galleryWrapper = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryWrapper.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryWrapper.addEventListener('click', imageOriginalSizeHandler);
+
+console.log(galleryWrapper);
+
+function imageOriginalSizeHandler(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="1280" height="855">`);
+    instance.show();
+
+    galleryWrapper.addEventListener('keydown', (event) => {
+        if (event.code === 'Escape') {
+            instance.close();
+        }
+    })
+}
+
